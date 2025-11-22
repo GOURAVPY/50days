73ad1c208ca695f764c0ffd1a0f0ed1726fcbb75
GOURAVPY <gouravsuman2006@gmail.com>
2025-11-22 17:59:17 +0530

    meun1

--- day3/script.js
+++ day3/script.js
@@ -7,24 +7,45 @@ let menu = {
   combo: 425,
 };
 
-function orderdisplay() {
+function orderdisplay(total) {
   console.log("-----------------------------------------------\n");
   console.log("    MENU - what you like to order ?    ");
   console.log("-----------------------------------------------\n");
 
   for (const item in menu) {
     console.log(`${item} : $${menu[item]}\n`);
-
   }
-
+  console.log("---------------------------------------------\n");
+  console.log(`CURRent TOTAL: $${total}\n`);
+  console.log('type anitem name (e.g. "combo") or "checkout" \n');
 }
 
 function startorder() {
+  let total = 0;
+  let orders = true;
+  console.log("===============================================\n");
+  console.log("       WELCOME TO FAST FOOD ORDERING SYSTEM     \n");
+  console.log("===============================================\n");
 
-    total = 0;
-    orders = true;
-    
-
-
+  while (orders) {
+    orderdisplay(total);
+    let userinput = prompt("Enter your order: ").toLowerCase();
+    if (userinput === "null") {
+      console.log("Thank you for visiting. Goodbye!");
+      orders = false;
+      break;
+    }
+    let order = userinput.toLowerCase().trim();
+    if (order === "checkout") {
+      orders = false;
+      break;
+    }
+    if (menu[order]) {
+      const itemprice = menu[order];
+      total += itemprice;
+      console.log(
+        `You have added ${order} to your order. $${itemprice} has been added to your total.\n`
+      );
+    }
+  }
 }
-
