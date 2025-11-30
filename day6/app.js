a6e30d164ea1dcfeab9d0185ed295a44e5a36d73
GOURAVPY <gouravsuman2006@gmail.com>
2025-11-30 16:30:48 +0530

    day 6

---
+++ day6/app.js
@@ -0,0 +1,13 @@
+const timelen = 60;
+let time = timelen * 60;
+const diply = document.querySelector(".displey");
+setInterval(updeta, 1000);
+
+function updeta() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  diply.innerHTML = `${minutes}:${seconds}`;
+  console.log(`${minutes}:${seconds}`);
+  time--;
+}
